65d21aa5ae23d2d0bd05ac084469471f4cb24a9b
Yuriy Halas <jurahalas@gmail.com>
2017-11-19 21:48:48 +0200

    clean code

--- src/AwesomeButton.js
+++ src/AwesomeButton.js
@@ -1,4 +1,4 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component} from 'react';
 import { Animated, View } from 'react-native';
 
 import ButtonView from './ButtonView';
